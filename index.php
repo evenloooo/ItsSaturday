3ccc5dd992a6c6c881c672337d5ad84784215d04
Even Lo <even.buy@gmail.com>
2016-04-29 11:31:27 +0800

    update vpon

--- index.php
+++ index.php
@@ -4,49 +4,53 @@ $is_mobile = preg_match('/android|webos|iphone|ipad|ipod|blackberry|iemobile|ope
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta charset="UTF-8">
-    <meta name="description" content="用手機來控制電腦上的影片吧！" />
-    <meta name="author" content="EvenLo" />
-    <title>Vpon's task</title>
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="UTF-8">
+  <meta name="description" content="用手機來控制電腦上的影片吧！" />
+  <meta name="author" content="EvenLo" />
+  <meta property="og:image" content="assets/img/tour.png" />
+  <meta property="og:image:type" content="image/jpeg" />
+  <meta property="og:url" content="http://creative.even.pw/"/>
+  <meta property="og:title" content="向左轉，向右轉。"/>
+  <title>Vpon's task</title>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-      ga('create', 'UA-55909698-3', 'auto');
-      ga('send', 'pageview');
+    ga('create', 'UA-55909698-3', 'auto');
+    ga('send', 'pageview');
 
   </script>
 </head>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 <body class="container">
-    <style type="text/css">
-       html, body {
-         margin: 0;
-         padding: 0;
-         width: 100%;
-         height: 100%;
-         display: table;
-         text-align: center;
-     }
-     .mobileTitle {
-        display: table-cell;
-        text-align: center;
-        vertical-align: middle;
-    }
-    #player2 {
-        display: none;
-    }
-    h2,h4 {
+  <style type="text/css">
+   html, body {
+     margin: 0;
+     padding: 0;
+     width: 100%;
+     height: 100%;
+     display: table;
+     text-align: center;
+   }
+   .mobileTitle {
+    display: table-cell;
+    text-align: center;
+    vertical-align: middle;
+  }
+  #player2 {
+    display: none;
+  }
+  h2,h4 {
 
-        text-align: center;
-    }
-    iframe, div {
-        display: table;
-        margin: 0 auto;
-    }
+    text-align: center;
+  }
+  iframe, div {
+    display: table;
+    margin: 0 auto;
+  }
 </style>
 <?if(!$is_mobile) {?>
 <h2>Vpon's task</h2>
@@ -55,13 +59,13 @@ $is_mobile = preg_match('/android|webos|iphone|ipad|ipod|blackberry|iemobile|ope
 <iframe src="https://player.vimeo.com/video/83602545?autoplay=1" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen id="player1"></iframe>
 <iframe src="https://player.vimeo.com/video/83602546" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen id="player2"></iframe>
 <div>
-    <h3>用你的手機來控制影片吧！</h3>
-    <h5>用手機打開網址 <a href="http://creative.even.pw/">http://creative.even.pw/</a>，透過旋轉來控制影片</h5>
-    <img src="assets/img/tour.png" style="width:50%;">
+  <h3>用你的手機來控制影片吧！</h3>
+  <h5>用手機打開網址 <a href="http://creative.even.pw/">http://creative.even.pw/</a>，透過旋轉來控制影片</h5>
+  <img src="assets/img/tour.png" style="width:50%;">
 </div>
 <?} else {?>
 <h3 class="mobileTitle">
-   向左轉，向右轉。
+ 向左轉，向右轉。
 </h3>
 <?}?>
 
